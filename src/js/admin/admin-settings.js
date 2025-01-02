9353643fdf07f688b9e45b630d827ef62ae22409
Eduardo <dev@eduardovillao.me>
2025-01-02 20:55:11 -0300

    feat: move files to src + add build files

--- src/js/admin/admin-settings.js
+++ src/js/admin/admin-settings.js
@@ -4,12 +4,14 @@ function mydChangeTab(e) {
 	tabContent = document.querySelectorAll('.myd-tabs-content'),
 	clicked = e.target;
 
-	tabs.forEach(item => { 
+	tabs.forEach(item => {
 		item.classList.remove('nav-tab-active');
 	});
-	tabContent.forEach(item => { 
+	tabContent.forEach(item => {
 		item.classList.remove('myd-tabs-content--active');
 	});
 	clicked.classList.add('nav-tab-active');
 	document.getElementById(clicked.id + '-content').classList.add('myd-tabs-content--active');
 }
+
+window.mydChangeTab = mydChangeTab;
--- src/js/front-js.js
+++ src/js/front-js.js
@@ -8,12 +8,12 @@ jQuery(document).ready(function($) {
                     var var_phone = $('#woapp_number').val();
                     var variable_price = $('.variations_form.cart .amount').text();
                     var var_href = 'https://wa.me/' + var_phone + '?text=' + var_message + '%0D%0A%0D%0A' + var_name + '%0D%0A' + variable_price + '%0D%0A' + var_link;
-                    $(".evowap_btn").attr("href", var_href);   
+                    $(".evowap_btn").attr("href", var_href);
                 }
                 if( '' != $('input.variation_id').val() && '' == $('.woocommerce-variation-price').text() ) {
                     var regular_price = $('#woapp_reg_price').val();
                     var var_href2 = 'https://wa.me/' + var_phone + '?text=' + var_message + '%0D%0A%0D%0A' + var_name + '%0D%0A' + regular_price + '%0D%0A' + var_link;
-                    $(".evowap_btn").attr("href", var_href2);   
+                    $(".evowap_btn").attr("href", var_href2);
                 }
             });
 });
