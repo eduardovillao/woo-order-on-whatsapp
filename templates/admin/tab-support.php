7bf2ad8434aaa9ab2e7af0651eee371c7d290c8d
Eduardo <dev@eduardovillao.me>
2025-01-03 00:56:10 -0300

    chore: add free support link

--- templates/admin/tab-support.php
+++ templates/admin/tab-support.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	<div class="card">
 		<h3><?php esc_html_e( 'Do you need support?', 'woo-order-on-whatsapp' );?></h3>
-		<p><?php esc_html_e( 'Hey, thank you for using our plugin! If you need anything, contact us on', 'woo-order-on-whatsapp' );?> <a href="https://codecanyon.net/item/woo-order-on-whatsapp/25824812/support" target="_blank"><?php esc_html_e( 'Premium Support', 'woo-order-on-whatsapp' );?>.</a></p>
+		<p><?php esc_html_e( 'Hey, thank you for using our plugin! If you need anything, contact us on', 'woo-order-on-whatsapp' );?> <a href="https://wordpress.org/support/plugin/woo-order-on-whatsapp/" target="_blank"><?php esc_html_e( 'Support', 'woo-order-on-whatsapp' );?>.</a></p>
 	</div>
 
 </div>
